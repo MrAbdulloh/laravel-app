91fa2fa0407b34b26def5c63c325c15ca16fe90c
Abdulloh <akramovabdullo2002@gmail.com>
2023-01-18 22:26:20 +0300

    add Model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'body', 'img', 'slug'];
+//    protected $guarded = []
+    public function comments()
+    {
+        // one to many
+        return $this->hasMany(Comment::class);
+    }
+
+    public function stats()
+    {
+        //one to one
+        return $this->hasOne(State::class);
+    }
+
+    public function tags()
+    {
+        // many to many
+        return $this->belongsToMany(Tag::class);
+    }
+
 }
